496db6338a6668a83884138a3d117a77c6270a66
DmitriyKondrahin <135246004+DmitriyKondrahin@users.noreply.github.com>
2023-06-08 17:12:29 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -4,6 +4,14 @@ win = display.set_mode((600, 500))
 display.set_caption('Ping-pong')
 win.fill((255, 255, 255))
 
+font.init()
+font = font.Font(None, 35)
+lose_1 = font.render('player 1 lose!', True, (100, 0, 0))
+lose_2 = font.render('player 2 lose!', True, (100, 0, 0))
+
+speed_x = 4
+speed_y = 4
+
 a = True
 finish = False
 
@@ -32,9 +40,32 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 550:
             self.rect.y += self.speed
 
+myach = GameSprite("w.png", 250, 250, 6, (50, 50))
+player1 = Player("w2.png", 50, 100, 10, (30, 150))
+player2 = Player("w2.png", 550, 100, 10, (30, 150))
+
 while a:
     for e in event.get():
         if e.type == QUIT:
             a = False
+    if finish != True:
+        win.fill((255, 255, 255))
+        myach.rect.x += speed_x
+        myach.rect.y += speed_y
+        player1.reset()
+        player1.update_l()
+        player2.reset()
+        player2.update_r()
+        myach.reset()
+    if myach.rect.y > 450 or myach.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(player1, myach) or sprite.collide_rect(player2, myach):
+        speed_x *= -1
+    if myach.rect.x > 550:
+        finish = True
+        win.blit(lose_2, (200, 200))
+    if myach.rect.x < 0:
+        finish = True
+        win.blit(lose_1, (200, 200))
     display.update()
     time.delay(50)
